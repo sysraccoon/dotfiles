7ae0636f05709fa85fc27cf364bb8214c8c4cb0c
sysraccoon <sysraccoon@gmail.com>
2021-12-22 13:33:00 +0000

    antigen cleanup

--- zsh/modules/antigen.zsh
+++ zsh/modules/antigen.zsh
@@ -2,15 +2,15 @@ ANTIGEN_DIR="$ZSH_CONFIG_DIR/antigen"
 mkdir -p $ANTIGEN_DIR;
 
 if ! [ -f "$ANTIGEN_DIR/antigen.zsh" ]; then
-  echo "Error: antigen missed. Try installing...";
+  echo "Warning: antigen missed. Try installing...";
   curl -vL git.io/antigen > "$ANTIGEN_DIR/antigen.zsh";
 fi
 
 source "$ANTIGEN_DIR/antigen.zsh";
 
-if [ -f "$ANTIGEN_DIR/antigenrc" ]; then
-  antigen init "$ANTIGEN_DIR/antigenrc";
-else
-  echo "Error: $ANTIGEN_DIR/antigenrc missed";
-fi
+antigen bundle git
+antigen bundle hcgraf/zsh-sudo
+antigen bundle Aloxaf/fzf-tab
+antigen bundle zsh-users/zsh-syntax-highlighting
 
+antigen apply
