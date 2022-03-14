48b010ad1d8c9cd0c7c5098e36d81d1033bdd157
sysraccoon <sysraccoon@gmail.com>
2022-03-15 00:40:58 +0300

    add yay aliases

--- zsh/modules/pacman.zsh
+++ zsh/modules/pacman.zsh
@@ -1,2 +1,2 @@
-alias pac="pacman -Slq | fzf --multi --exact --cycle --reverse --preview 'pacman -Si {1}' | xargs -ro sudo pacman -S"
+alias pac-i="pacman -Slq | fzf --multi --exact --cycle --reverse --preview 'pacman -Si {1}' | xargs -ro sudo pacman -S"
 alias pac-up="pacman -Syu"
