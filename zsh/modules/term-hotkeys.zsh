f30cb4596bfbfdc1952d0331cb5607d1823ca098
sysraccoon <sysraccoon@gmail.com>
2022-11-10 17:49:42 +0300

    add show-command-source zsh keybinding

--- zsh/modules/term-hotkeys.zsh
+++ zsh/modules/term-hotkeys.zsh
@@ -5,9 +5,15 @@ bindkey '^[f' forward-char
 bindkey '^b' backward-word
 bindkey '^[b' backward-char
 
-function yank-current-buffer {
+function __yank-current-buffer {
     echo "$BUFFER" | cb-set
 }
-zle -N yank-current-buffer
-bindkey "\ey" yank-current-buffer
+zle -N __yank-current-buffer
+bindkey "\ey" __yank-current-buffer
+
+function __show-command-source {
+    bat --paging always --pager "less -Rc" =$(echo "$BUFFER" | awk '{print $1;}')
+}
+zle -N __show-command-source
+bindkey "\es" __show-command-source
 
