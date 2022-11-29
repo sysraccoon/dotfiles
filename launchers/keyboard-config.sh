2a7a2b1e82166c13196342e0810a666ff787aa75
sysraccoon <sysraccoon@gmail.com>
2022-11-29 18:04:15 +0300

    bind single press Alt_R to Enter

--- launchers/keyboard-config.sh
+++ launchers/keyboard-config.sh
@@ -2,4 +2,4 @@
 setxkbmap -layout "us,ru" -variant "dvorak," -option "grp:alt_shift_toggle,ctrl:nocaps,altwin:swap_lalt_lwin"
 
 killall xcape
-xcape -e 'Control_L=Escape;Shift_L=Super_L|bracketleft;Shift_R=Super_L|bracketright' -t 200
+xcape -e 'Control_L=Escape;Shift_L=Super_L|bracketleft;Shift_R=Super_L|bracketright;Alt_R=Return' -t 200
