fb26bb1775f7cef3240195547745491501e20b5c
sysraccoon <sysraccoon@gmail.com>
2023-02-03 17:33:55 +0300

    add source to profile.d/nix.sh

--- configs/zsh/modules/nix.zsh
+++ configs/zsh/modules/nix.zsh
@@ -6,3 +6,7 @@ alias hm-up='home-manager switch'
 alias ehm='home-manager edit'
 
 source "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+
+if [ -f "$HOME/.nix-profile/etc/profile.d/nix.sh" ]; then
+    source "$HOME/.nix-profile/etc/profile.d/nix.sh"
+fi
