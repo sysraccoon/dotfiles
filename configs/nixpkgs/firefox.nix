f23a41adeb4f7755001268d8fc46970bf936c6c9
sysraccoon <sysraccoon@gmail.com>
2023-02-26 22:23:45 +0300

    add firefox screencast profile

--- configs/nixpkgs/firefox.nix
+++ configs/nixpkgs/firefox.nix
@@ -1,31 +1,50 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
+let 
+  firefoxProfile = { id, name, settings ? {}, default ? false }:
+  {
+    id = id;
+    name = name;
+    isDefault = default;
+    settings = {
+      "app.update.auto" = false;
+      "signon.rememberSignons" = false;
+      "browser.startup.homepage" = "about:blank";
+      "browser.newtabpage.enabled" = false;
+      "browser.toolbars.bookmarks.visibility" = "never";
+      "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+      "dom.textMetrics.fontBoundingBox.enabled" = true;
+    } // settings;
+    userChrome = builtins.readFile ../firefox/chrome/userChrome.css;
+    extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+      ublock-origin
+      vimium
+      kristofferhagen-nord-theme
+    ];
+  };
+in
 {
   nixpkgs.config.packageOverrides = pkgs: {
     nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
       inherit pkgs;
     };
   };
+
   programs.firefox = {
     enable = true;
     profiles = {
-      main = {
+      main = firefoxProfile {
         id = 0;
+        name = "main";
+        default = true;
+      };
+      screencast = firefoxProfile {
+        id = 1;
+        name = "screencast";
         settings = {
-          "app.update.auto" = false;
-          "signon.rememberSignons" = false;
-          "browser.startup.homepage" = "about:blank";
-          "browser.newtabpage.enabled" = false;
-          "browser.toolbars.bookmarks.visibility" = "never";
-          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+          "layout.css.devPixelsPerPx" = 1.5;
         };
-        userChrome = builtins.readFile ../firefox/chrome/userChrome.css;
       };
     };
-    extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-      ublock-origin
-      vimium
-      kristofferhagen-nord-theme
-    ];
   };
 }
