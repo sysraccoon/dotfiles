ae5e588ba6c9c98bb8e648766e803cbd3d16bdc7
sysraccoon <sysraccoon@gmail.com>
2023-03-04 10:22:48 +0300

    update nix.zsh modules

--- configs/zsh/modules/nix.aliases.zsh
+++ configs/zsh/modules/nix.aliases.zsh
@@ -1,6 +1,7 @@
 
 alias nix-search-cache='nix-env -qa --json > /tmp/.nix-search-cache'
-alias nix-search='nix search nixpkgs'
+alias nix-search='nix search nixpkgs --offline'
 alias nixos-up='nixos-rebuild switch'
 alias hm='home-manager'
 alias hm-up='home-manager switch'
+alias nix-sp='nix-shell --run zsh -p'
--- configs/zsh/modules/nix.zsh
+++ configs/zsh/modules/nix.zsh
@@ -1,12 +1,12 @@
 export NIX_PATH="$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}"
 
-alias enixos="nvim /etc/nixos +':cd %' +':Telescope find_files'"
-alias nixos-up='nixos-rebuild switch'
-alias hm-up='home-manager switch'
-alias ehm='home-manager edit'
-
 source "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
 
 if [ -f "$HOME/.nix-profile/etc/profile.d/nix.sh" ]; then
     source "$HOME/.nix-profile/etc/profile.d/nix.sh"
 fi
+
+if [ -f "$HOME/.nix-profile/etc/profile.d/command-not-found.sh" ]; then
+    source "$HOME/.nix-profile/etc/profile.d/command-not-found.sh"
+fi
+
