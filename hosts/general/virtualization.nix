ec25d7a3c8146342cfd0b08b3bb1c187266c7762
sysraccoon <sysraccoon@gmail.com>
2024-05-10 22:11:54 +0300

    update virtualisation settings

--- hosts/general/virtualization.nix
+++ hosts/general/virtualization.nix
@@ -16,6 +16,15 @@
     '';
   };
 
-  virtualisation.libvirtd.enable = true;
-  virtualisation.spiceUSBRedirection.enable = true;
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemu = {
+        swtpm.enable = true;
+        ovmf.enable = true;
+        ovmf.packages = [ pkgs.OVMFFull.fd ];
+      };
+    };
+    spiceUSBRedirection.enable = true;
+  };
 }
