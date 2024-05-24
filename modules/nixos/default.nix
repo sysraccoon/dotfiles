80be0f69cd6541f84034d1b691ed06e4f1b82678
sysraccoon <sysraccoon@gmail.com>
2024-05-24 19:51:59 +0300

    add syncthing module

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -3,9 +3,11 @@ rec {
     imports = [
       i18n
       nix
+      syncthing
     ];
   };
 
   i18n = import ./i18n.nix;
   nix = import ./nix.nix;
+  syncthing = import ./syncthing.nix;
 }
---
+++ modules/nixos/syncthing.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, lib, ... }:
+let
+  cfg = config.sys.nixos.syncthing;
+in {
+  options.sys.nixos.syncthing = {
+    enable = lib.mkEnableOption "toggle custom syncthing setup";
+    user = lib.mkOption {
+      type = lib.types.str;
+      description = "user that run syncthing";
+    };
+    configDir = lib.mkOption {
+      type = lib.types.str;
+      default = "/home/${cfg.user}/.config/syncthing";
+    };
+    dataDir = lib.mkOption {
+      type = lib.types.str;
+      default = "${cfg.configDir}/db";
+    };
+    openDefaultPorts = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Open default ports that used by syncthing";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.syncthing = {
+      enable = true;
+      user = "raccoon";
+      configDir = cfg.configDir;
+      dataDir = cfg.dataDir;
+    };
+
+    networking.firewall = lib.mkIf cfg.openDefaultPorts {
+      allowedTCPPorts = [ 8384 22000 ];
+      allowedUDPPorts = [ 22000 21027 ];
+    };
+  };
+}
