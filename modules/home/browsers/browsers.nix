be3cccced0bf875205b5f58dabf37a9a8d2bf7f1
sysraccoon <sysraccoon@gmail.com>
2024-06-09 17:23:38 +0300

    add chromium module

--- modules/home/browsers/browsers.nix
+++ modules/home/browsers/browsers.nix
@@ -1,11 +1,13 @@
 rec {
   default = {
     imports = [
+      chromium
       firefox
       qutebrowser
     ];
   };
 
+  chromium = import ./chromium/chromium.nix;
   firefox = import ./firefox/firefox.nix;
   qutebrowser = import ./qutebrowser/qutebrowser.nix;
 }
---
+++ modules/home/browsers/chromium/chromium.nix
@@ -0,0 +1,30 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.sys.home.browsers.chromium;
+in {
+  options.sys.home.browsers.chromium = {
+    enable = lib.mkEnableOption "toggle custom chromium setup";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.chromium = {
+      enable = true;
+      commandLineArgs = [
+        "--disable-new-avatar-menu"
+        "--high-dpi-support=1"
+        "--force-device-scale-factor=1.5"
+        "--ozone-platform-hint=auto"
+        "--ozone-platform=wayland"
+      ];
+      extensions = [
+        # vimium
+        {id = "dbepggeogbaibhgnhhndojpepiihcmeb";}
+        # adblock
+        {id = "cfhdojbkjhnklbpkdaibdccddilifddb";}
+      ];
+    };
+  };
+}
